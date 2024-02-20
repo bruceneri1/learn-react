35ebbec3fa6836db61b751e9e3e9b6a62c7dec1c
bruceneri1 <105466028+bruceneri1@users.noreply.github.com>
2024-02-20 11:58:04 -0500

    Update shoppingCart.js

--- src/pages/qcomps/shoppingCart.js
+++ src/pages/qcomps/shoppingCart.js
@@ -5,6 +5,7 @@ export default function RequestTracker() {
   const [completed, setCompleted] = useState(0);
 
   async function handleClick() {
+
     setPending(pending + 1);
     await delay(3000);
     setPending(pending - 1);
